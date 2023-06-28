50bf02d3462fb326dc5074b59c35caa440afe580
manaswii <manaswi.dewan@gmail.com>
2023-06-28 16:28:55 +0530

    added mongodb compass

--- Models/School.js
+++ Models/School.js
@@ -1,24 +1,26 @@
 const mongoose = require('mongoose');
 
 const schoolSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  address: String,
-  city: String,
-  postalCode: String,
-  state: String,
-  website: { type: String, match: /^https?:\/\//i },
-  email: { type: String, match: /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/ },
-  phoneNumber: String,
-  board: String,
-  fee: {
-    minimum: { type: Number, min: 0 },
-    maximum: { type: Number, min: 0 }
-  },
-  googleMap: { type: String, match: /^https?:\/\//i },
-  overview: String,
-  photo: String
-});
-
-const School = mongoose.model('schools', schoolSchema);
-
-module.exports = School;
+    _id: mongoose.Schema.Types.ObjectId, // Add this line for the _id field
+    name: { type: String, required: true },
+    address: String,
+    city: String,
+    postalCode: String,
+    state: String,
+    website: { type: String, match: /^https?:\/\//i },
+    email: { type: String, match: /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/ },
+    phoneNumber: String,
+    board: String,
+    fee: {
+      minimum: { type: Number, min: 0 },
+      maximum: { type: Number, min: 0 }
+    },
+    googleMap: { type: String, match: /^https?:\/\//i },
+    overview: String,
+    "school photo urls" : [String] // Changed to an array of strings for photo URLs
+  });
+  
+  const School = mongoose.model('schools', schoolSchema);
+  
+  module.exports = School;
+  
--- index.js
+++ index.js
@@ -1,25 +1,51 @@
-const express = require('express')
+const express = require('express');
 const mongoose = require("mongoose");
 const cors = require("cors");
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
+
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://ConnectSchools:7UPYJ34g4xq8coA8@connectschools.cvofo90.mongodb.net/?retryWrites=true&w=majority";
+
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
+
+
+
 
 // const mongoose = require('mongoose');
 const School = require('./Models/school');
 
 // Connect to MongoDB
 
-let uri = 'mongodb://127.0.0.1:27017/connectschools'
+let uri = 'mongodb+srv://ConnectSchools:7UPYJ34g4xq8coA8@connectschools.cvofo90.mongodb.net/connectschools?retryWrites=true&w=majority';
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    console.log('Connected to MongoDB');
+      console.log('Connected to MongoDB');
   })
   .catch((error) => {
     console.error('Failed to connect to MongoDB:', error);
   });
-
-
-
 app.use(cors());
 
 //routes
@@ -28,11 +54,16 @@ app.get('/', (req, res) => {
 })
 
 app.get('/getSchools', async (req, res) => {
-    let query = req.query;
-    console.log(query);
-    let schools = await School.find().limit(100);
-    res.send(schools);
-})
+    try {
+        console.log(req.query)
+        const schools = await School.find({district : req.query['district']}).limit(1);
+        console.log(schools);
+        res.send(schools);
+      } catch (error) {
+        console.error('Error retrieving schools:', error);
+        res.status(500).send('Failed to retrieve schools');
+      }
+    });
 
 
 // autocomplete API endpoint
