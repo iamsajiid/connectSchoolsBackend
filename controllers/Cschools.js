56a82b3d5fae318f65f4b163a53d301c03585ad4
sajid <iamsajiid@gmail.com>
2023-07-08 15:00:20 +0530

    refactored code

--- controllers/Cschools.js
+++ controllers/Cschools.js
@@ -53,11 +53,11 @@ const getAllSchools = async (req,res) => {
         result = result.select(fieldsList)
     }
 
-    // const page = Number(req.query.page) || 1
-    // const limit = Number(req.query.limit) || 10
-    // const skip = (page-1) * limit
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const skip = (page-1) * limit
 
-    // result = result.skip(skip).limit(limit)
+    result = result.skip(skip).limit(limit)
 
     try {
         const school = await result
