0f22a9b68d70c9fe018577927bce93b5c5f6bc1a
Manaswi Sharma <85053597+manaswii@users.noreply.github.com>
2023-07-22 13:55:25 +0530

    slight alternation in order of app.use()

--- app.js
+++ app.js
@@ -22,8 +22,8 @@ app.use(session({
     saveUninitialized: false
   }));
 app.use(cors())
-app.use('/', routes)
 app.use(express.json())
+app.use('/', routes)
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
 
